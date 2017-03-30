6b0de7ece5f07b6afaaed30d0b2b0c6651cd7439
Lukasz Rodak <luki.rodak@op.pl>
2017-03-30 09:17:08 +0200

    Wyjdz i nie wracaj

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,8 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_Quit_clicked()
+{
+    close();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_Quit_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
