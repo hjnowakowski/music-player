44676335bd3d0ccd9257bfdb03fa46754cf0f4fd
hjnowakowski <hjnowakowski@gmail.com>
2017-05-27 17:27:11 +0200

    Działające drag and drop, program wyświetla link do pliku

--- build-projekt1_rozgrzewka1-Desktop_Qt_5_8_0_clang_64bit-Debug/moc_drag_n_drop.cpp
+++ build-projekt1_rozgrzewka1-Desktop_Qt_5_8_0_clang_64bit-Debug/moc_drag_n_drop.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_drag_n_drop_t {
-    QByteArrayData data[1];
-    char stringdata0[12];
+    QByteArrayData data[4];
+    char stringdata0[69];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -31,10 +31,14 @@ struct qt_meta_stringdata_drag_n_drop_t {
     )
 static const qt_meta_stringdata_drag_n_drop_t qt_meta_stringdata_drag_n_drop = {
     {
-QT_MOC_LITERAL(0, 0, 11) // "drag_n_drop"
+QT_MOC_LITERAL(0, 0, 11), // "drag_n_drop"
+QT_MOC_LITERAL(1, 12, 26), // "on_pushButton_open_clicked"
+QT_MOC_LITERAL(2, 39, 0), // ""
+QT_MOC_LITERAL(3, 40, 28) // "on_pushButton_delete_clicked"
 
     },
-    "drag_n_drop"
+    "drag_n_drop\0on_pushButton_open_clicked\0"
+    "\0on_pushButton_delete_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -44,21 +48,35 @@ static const uint qt_meta_data_drag_n_drop[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x08 /* Private */,
+       3,    0,   25,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void drag_n_drop::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        drag_n_drop *_t = static_cast<drag_n_drop *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton_open_clicked(); break;
+        case 1: _t->on_pushButton_delete_clicked(); break;
+        default: ;
+        }
+    }
     Q_UNUSED(_a);
 }
 
@@ -86,6 +104,15 @@ int drag_n_drop::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     _id = QMainWindow::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
     return _id;
 }
 QT_WARNING_POP
--- drag_n_drop.h
+++ drag_n_drop.h
@@ -15,6 +15,19 @@ public:
     explicit drag_n_drop(QWidget *parent = 0);
     ~drag_n_drop();
 
+protected:
+
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dragLeaveEvent(QDragLeaveEvent *event);
+    void dragMoveEvent(QDragMoveEvent *event);
+    void dropEvent(QDropEvent *event);
+
+
+private slots:
+    void on_pushButton_open_clicked();
+
+    void on_pushButton_delete_clicked();
+
 private:
     Ui::drag_n_drop *ui;
 };
